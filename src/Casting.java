79120554beec727929f4ff61b2022f5aff3bfe98
rahuldhirendersingh <rahuldhirendersingh@gmail.com>
2026-06-25 10:20:15 +0530

    basics of core java

---
+++ src/Casting.java
@@ -0,0 +1,12 @@
+public class Casting {
+    public static void main(String[] args) {
+        // type casting ?
+
+        float num = 9; // implicit casting
+        System.out.println(num);
+
+        int num2 = (int) 9.5F; // explicit casting
+        System.out.println(num2);
+
+    }
+}
